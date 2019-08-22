40b1db173ef32b0a0d13f26b4c8c1b2fde2a430c
Mystery2099 <54286204+Mystery2099@users.noreply.github.com>
2019-08-22 00:50:51 -0400

    Add files via upload

---
+++ main/java/com/Mystery2099/marblemod/marblemod/Main.java
@@ -0,0 +1,48 @@
+package com.Mystery2099.marblemod;
+
+import com.Mystery2099.marblemod.proxy.CommonProxy;
+import com.Mystery2099.marblemod.tabs.MarbleTab;
+import com.Mystery2099.marblemod.util.Reference;
+import com.Mystery2099.marblemod.world.ModWorldGen;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main 
+{
+	
+	//Tabs
+	public static final CreativeTabs MARBLETAB = new MarbleTab("marbletab");
+	
+	@Instance
+	public static Main instance;
+	
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
+	public static CommonProxy proxy;
+	
+	@EventHandler
+	public static void PreInit(FMLPreInitializationEvent event)
+	{
+		GameRegistry.registerWorldGenerator(new ModWorldGen(),  3);
+	}
+	
+	@EventHandler
+	public static void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public static void PosteInit(FMLInitializationEvent event)
+	{
+		
+	}
+
+}
