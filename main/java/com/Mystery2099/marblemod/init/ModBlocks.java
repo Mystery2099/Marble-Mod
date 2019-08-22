16914e007f5471af790aec30e89d7c4b06b4a831
Mystery2099 <54286204+Mystery2099@users.noreply.github.com>
2019-08-22 00:52:03 -0400

    Add files via upload

--- main/java/com/Mystery2099/marblemod/init/ModBlocks.java
+++ main/java/com/Mystery2099/marblemod/init/ModBlocks.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.Mystery2099.marblemod.blocks.BlockBase;
+import com.Mystery2099.marblemod.blocks.EarthQuartzBlock;
 import com.Mystery2099.marblemod.blocks.LimestoneBlock;
 import com.Mystery2099.marblemod.blocks.MarbleBlock;
 
@@ -16,4 +17,5 @@ public class ModBlocks
 	
 	public static final Block LIMESTONE_BLOCK = new LimestoneBlock("limestone_block", Material.ROCK);
 	public static final Block MARBLE_BLOCK = new MarbleBlock("marble_block", Material.ROCK);
+	public static final Block EARTH_QUARTZ_ORE = new EarthQuartzBlock("earth_quartze_ore", Material.ROCK);
 }
